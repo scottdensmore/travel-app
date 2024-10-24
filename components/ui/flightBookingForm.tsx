3ab09c024dc3a97e5eea400082da27a926c2c82a
Tim Rogers <me@timrogers.co.uk>
2024-10-24 08:56:47 -0700

    Default to SFO-NYC on the homepage, not NYC-NYC

--- components/ui/flightBookingForm.tsx
+++ components/ui/flightBookingForm.tsx
@@ -80,13 +80,13 @@ const FlightBookingForm: React.FC = () => {
                   <div className="fields-container">
                   <label htmlFor="from">From</label>
                   <select id="from" name="from">
+                    <option value="SFO">San Francisco (SFO)</option>
                     <option value="NYC">New York City (NYC)</option>
                     <option value="LAX">Los Angeles (LAX)</option>
                     <option value="CHI">Chicago (CHI)</option>
                     <option value="ATL">Atlanta (ATL)</option>
                     <option value="DFW">Dallas/Fort Worth (DFW)</option>
                     <option value="DEN">Denver (DEN)</option>
-                    <option value="SFO">San Francisco (SFO)</option>
                     <option value="SEA">Seattle (SEA)</option>
                     <option value="LAS">Las Vegas (LAS)</option>
                     <option value="MIA">Miami (MIA)</option>
